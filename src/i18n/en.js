68bf9621a1db61166eb0ea301f4db0f5357d8fa2
quytm <minhquylt95@gmail.com>
2019-02-21 10:07:18 +0700

    booking-history: i18n for booking list

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -4,6 +4,14 @@ import React from "react";
 export const ENGLISH = {
     lang: "en",
     messages: {
+
+        // Common
+        "carrier": "Carrier",
+        "date": "Date",
+        "search": "Search",
+        "clear": "Clear",
+
+        // Menu
         "menu.login": "Login",
         "menu.register": "Register",
         "menu.booking": "Booking",
@@ -14,6 +22,8 @@ export const ENGLISH = {
         "menu.supplies": "Supplies",
         "menu.invoices": "Invoices",
         "menu.help": "Help",
+
+        // Booking
         "booking.senderAddress": "Sender Address",
         "booking.company": "Company",
         "booking.phone": "Phone",
@@ -37,5 +47,34 @@ export const ENGLISH = {
         "booking.packageShipment": "Package and Shipment Details",
         "booking.continueBooking": "Continue Booking",
         "booking.quote": "Quote",
+
+        // Booking History
+        "history.history": "History",
+        "history.filterRange.today": "Show Today Only",
+        "history.filterRange.last30Ds": "Show Last 30 Days",
+        "history.filterRange.last60Ds": "Show Last 60 Days",
+        "history.filterRange.last90Ds": "Show Last 90 Days",
+        "history.filterRange.range": "Show Range",
+        "history.filterRange.all": "Show All",
+        "history.refresh": "Refresh",
+        "history.connoteNumber": "Connote Number",
+        "history.senderCity": "Sender City",
+        "history.receiverCity": "Receiver City",
+        "history.senderName": "Sender Name",
+        "history.receiveName": "Receive Name",
+        //      // header table
+        "history.scheduleCollectionTimestamp": "Schedule Collection Timestamp",
+        "history.collectionInfo": "Collection Information",
+        "history.destination": "Destination",
+        "history.destCountry": "Dest. Country",
+        "history.voided": "Voided",
+        "history.tracking": "Tracking",
+        "history.timestamp": "Timestamp",
+        "history.shipDate": "Ship Date",
+        "history.pieces": "Pieces",
+        "history.service": "Service",
+        "history.weight": "Weight",
+        "history.quoted": "Quoted",
+        "history.scheduled": "Scheduled",
     }
 };
