ab84b5146330b9c261a202587d7aebb50d9dd5a0
quytm <minhquylt95@gmail.com>
2019-02-25 09:49:16 +0700

    booking-history: add dateFormat util

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -3,6 +3,10 @@ const dateFormat = {
         const d = new Date(date);
         return d.getDate() + '/' + (d.getMonth() + 1) + '/' + d.getFullYear();
     },
+    ddmmyyyyMinus: (date) => {
+        const d = new Date(date);
+        return `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`;
+    },
     ddmmyyyyHHMM: (date) => {
         const d = new Date(date);
         return d.toLocaleDateString("en-GB",{ year: 'numeric', month: 'numeric', day: 'numeric', hour:'numeric',minute:'numeric' });
